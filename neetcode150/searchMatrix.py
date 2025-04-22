8330227e825dd3f80a0e5cd95013bb5af4a66e8d
natashalukmanto <natshaaudrey@gmail.com>
2025-04-22 10:12:17 -0700

    add one-pass solution to Search Matrix

--- neetcode150/searchMatrix.py
+++ neetcode150/searchMatrix.py
@@ -1,5 +1,7 @@
 from typing import List
-def searchMatrix(matrix: List[List[int]], target: int) -> bool:
+
+# Optimized, but not one pass
+def searchMatrix1(matrix: List[List[int]], target: int) -> bool:
     rows, cols = len(matrix) - 1, len(matrix[0]) - 1
     left, right, bottom, top = 0, cols, 0, rows
 
@@ -28,4 +30,21 @@ def searchMatrix(matrix: List[List[int]], target: int) -> bool:
         else:
             right = middle_col - 1
 
+    return False
+
+# One pass
+def searchMatrix(matrix: List[List[int]], target: int) -> bool:
+    rows, cols = len(matrix), len(matrix[0])
+    left, right = 0, rows * cols
+
+    while left < right:
+        middle = left + (right - left) // 2
+        row, col = middle // cols, middle % cols
+        if matrix[row][col] == target:
+            return True
+        elif matrix[row][col] > target:
+            right = middle
+        else:
+            left = middle + 1
+    
     return False
