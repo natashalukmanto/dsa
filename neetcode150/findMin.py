1d60c935459d74b64abe98e12a7980cb4e1d0a7b
natashalukmanto <natshaaudrey@gmail.com>
2025-04-26 12:49:08 -0700

    redo findMin

--- neetcode150/findMin.py
+++ neetcode150/findMin.py
@@ -15,3 +15,19 @@ def findMin(nums: List[int]) -> int:
                 left = middle + 1
             else:
                 right = middle - 1
+                
+# Spaced-Repetition
+def findMin2(nums: List[int]) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        middle = left + (right - left) // 2
+        print(nums[middle])
+        if nums[middle - 1] >= nums[middle]:
+            return nums[middle]
+        if nums[middle + 1] < nums[middle]:
+            return nums[middle + 1]
+        if nums[middle] > nums[-1]:
+            left = middle + 1
+        else:
+            right = middle - 1
