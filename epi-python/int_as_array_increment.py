241ca1a59ffccb91bf5c25efb2f506d5b96c71c3
natashalukmanto <natshaaudrey@gmail.com>
2025-04-25 21:37:48 -0700

    update 5.2 Increment An Arbitrary-Precision Integer

--- epi-python/int_as_array_increment.py
+++ epi-python/int_as_array_increment.py
@@ -1,17 +1,23 @@
 from typing import List
 
 def plus_one(A: List[int]) -> List[int]:
-    carry = 0
     A[-1] += 1
-    for i in reversed(range(len(A))):
-        A[i] = A[i] + carry
-        carry = 0
-        if A[i] == 10:
-            carry = 1
-            A[i] = 0
     
-    if A[0] == 0:
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10: return A
+        A[i - 1] += 1
+        A[i] = 0
+        
+    if A[0] == 10:
         A[0] = 1
         A.append(0)
-            
+        
     return A
+
+A = [1, 2, 9]
+print(plus_one(A)) # [1, 3, 0]
+B = [9, 9, 9, 9]
+print(plus_one(B)) # [1, 0, 0, 0, 0]
+C = [0, 0, 0, 0]
+print(plus_one(C)) # [0, 0, 0, 1]
+
