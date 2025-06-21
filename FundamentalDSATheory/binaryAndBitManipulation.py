49bc74f7859a4e00ef16e391ff3b199d27b586c0
natashalukmanto <natshaaudrey@gmail.com>
2025-06-21 09:53:16 -0700

    Binary & Bit Manipulation

---
+++ FundamentalDSATheory/binaryAndBitManipulation.py
@@ -0,0 +1,28 @@
+print(bin(5)[2:])
+
+binary_5 = '101'
+print(int(binary_5, 2))
+
+# Bitwise AND (&)
+print(5 & 7)
+
+# Bitwise OR (!)
+print(5 | 7)
+
+# Bitwise XOR (^)
+print(5 ^ 7)
+
+# Bitwise NOT (~)
+print(~5)
+
+# Bitwise left shift (<<)
+print(5 << 2)
+
+# Bitwise right shift (<<), (aritmetic and signed)
+print(5 >> 1)
+
+# Hexadecimals (base 16)
+print(hex(5)) #0x5
+print(hex(11))
+print(hex(25)) # 16 + 9
+print(hex(890))
