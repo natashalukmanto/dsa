0b6dab2d55ab54e8d968dee05007db7c9709773d
natashalukmanto <natshaaudrey@gmail.com>
2025-09-27 08:55:42 -0700

    refactor(epi): revisit 5.1 the dutch national flag problem

--- epi-python/dutch_national_flag.py
+++ epi-python/dutch_national_flag.py
@@ -7,15 +7,32 @@ But we can do better, let's aproach this question with a Two Pointer algorithm.
 
 def dutch_flag_partition(pivot_index: int, A) -> None:
     # A.sort()
-    left, equal, right = 0, 0, len(A) - 1
-    pivot = A[pivot_index]
-    while equal <= right:
-        if A[equal] > pivot:
-            A[equal], A[right] = A[right], A[equal]
-            right -= 1
-        elif A[equal] == pivot:
-            equal += 1
+    # === Own try ===
+    
+    s, e, b = 0, 0, len(A) - 1
+    
+    original = A[pivot_index]
+    while e <= b:
+        if A[e] < original:
+            A[e], A[s] = A[s], A[e]
+            s += 1
+            e += 1
+        elif A[e] > original:
+            A[e], A[b] = A[b], A[e]
+            b -= 1
         else:
-            A[equal], A[left] = A[left], A[equal]
-            left += 1
-            equal += 1
+            e += 1
+                 
+    # ==== Textbook Solution ===
+    # left, equal, right = 0, 0, len(A) - 1
+    # pivot = A[pivot_index]
+    # while equal <= right:
+    #     if A[equal] > pivot:
+    #         A[equal], A[right] = A[right], A[equal]
+    #         right -= 1
+    #     elif A[equal] == pivot:
+    #         equal += 1
+    #     else:
+    #         A[equal], A[left] = A[left], A[equal]
+    #         left += 1
+    #         equal += 1
