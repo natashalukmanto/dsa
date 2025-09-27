2ff3af99484ccd18c6f3167b26127f5f17d331d6
natashalukmanto <natshaaudrey@gmail.com>
2025-09-26 22:52:26 -0700

    feat(lld): complete parking lot problem

--- lld/parking_lot/parking_floor.py
+++ lld/parking_lot/parking_floor.py
@@ -1,2 +1,35 @@
+import threading
+from parking_spot import ParkingSpot
+from vehicle import Vehicle
+from vehicle_size import VehicleSize
+from typing import Dict, Optional
+from collections import defaultdict
+
 class ParkingFloor():
-    
+    def __init__(self, floor_number: int):
+        self.floor_number = floor_number
+        self.spots = {}
+        self._lock = threading.Lock()
+        
+    def add_spot(self, spot: ParkingSpot):
+        self.spots[spot.get_spot_id()] = spot
+        
+    def find_available_spot(self, vehicle: Vehicle) -> Optional[ParkingSpot]:
+        with self._lock:
+            available_spots = [
+                spot for spot in self.spots.values() if not spot.is_occupied_spot() and spot.can_fit_vehicle(vehicle)
+            ]
+            if available_spots:
+                available_spots.sort(key=lambda x: x.get_spot_size().value)
+                print(available_spots)
+                
+    def display_availability(self):
+        print(f"--- Floor {self.floor_number} Availability ---")
+        available_spots = defaultdict(int)
+        
+        for spot in self.spots.values():
+            if not spot.is_occupied_spot(): available_spots[spot.get_spot_size()] += 1
+            
+        for size in VehicleSize:
+            print(f"{size.value} spots: {available_spots[size]}")
+            
--- lld/parking_lot/parking_lot.py
+++ lld/parking_lot/parking_lot.py
@@ -1,6 +1,10 @@
 import threading
-from typing import Dict, List, Optional
 from parking_floor import ParkingFloor
+from parking_ticket import ParkingTicket
+from fee_strategy import FlatRateFeeStrategy, FeeStrategy
+from parking_strategy import NearestFirstStrategy, ParkingStrategy
+from vehicle import Vehicle
+from typing import List, Dict, Optional
 
 # The **ParkingLot** class follows the Singleton pattern to ensure only one instance of the parking lot exists. 
 # It maintains a list of levels and provides methods to park and unpark vehicles.
@@ -8,12 +12,16 @@ from parking_floor import ParkingFloor
 class ParkingLot:
     _instance = None
     _lock = threading.Lock()
-    
+
     def __init__(self):
         if ParkingLot._instance is not None:
-            raise Exception("This is a singleton class!")
-        self.levels: List[ParkingFloor] = []
-        
+            raise Exception("This class is a singleton!")
+        self.floors: List[ParkingFloor] = []
+        self.active_tickets: Dict[str, ParkingTicket] = {}
+        self.fee_strategy = FlatRateFeeStrategy()
+        self.parking_strategy = NearestFirstStrategy()
+        self._main_lock = threading.Lock()
+
     @staticmethod
     def get_instance():
         if ParkingLot._instance is None:
@@ -21,7 +29,38 @@ class ParkingLot:
                 if ParkingLot._instance is None:
                     ParkingLot._instance = ParkingLot()
         return ParkingLot._instance
-    
-    def add_level(self, floor: ParkingFloor) -> None:
-        self.levels.append(floor)
-    
+
+    def add_floor(self, floor: ParkingFloor):
+        self.floors.append(floor)
+
+    def set_fee_strategy(self, fee_strategy: FeeStrategy):
+        self.fee_strategy = fee_strategy
+
+    def set_parking_strategy(self, parking_strategy: ParkingStrategy):
+        self.parking_strategy = parking_strategy
+
+    def park_vehicle(self, vehicle: Vehicle) -> Optional[ParkingTicket]:
+        with self._main_lock:
+            spot = self.parking_strategy.find_spot(self.floors, vehicle)
+            if spot is not None:
+                spot.park_vehicle(vehicle)
+                ticket = ParkingTicket(vehicle, spot)
+                self.active_tickets[vehicle.get_license_plate()] = ticket
+                print(f"Vehicle {vehicle.get_license_plate()} parked at spot {spot.get_spot_id()}")
+                return ticket
+            else:
+                print(f"No available spot for vehicle {vehicle.get_license_plate()}")
+                return None
+
+    def unpark_vehicle(self, license_number: str) -> Optional[float]:
+        with self._main_lock:
+            ticket = self.active_tickets.pop(license_number, None)
+            if ticket is None:
+                print(f"Ticket not found for vehicle {license_number}")
+                return None
+
+            ticket.get_spot().unpark_vehicle()
+            ticket.set_exit_timestamp()
+            fee = self.fee_strategy.calculate_fee(ticket)
+            print(f"Vehicle {license_number} unparked from spot {ticket.get_spot().get_spot_id()}")
+            return fee
