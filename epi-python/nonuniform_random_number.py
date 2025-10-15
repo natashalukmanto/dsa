be66118e04a5eaeb7c1c34797ef4ec017a329836
natashalukmanto <natshaaudrey@gmail.com>
2025-10-15 12:03:35 -0700

    nit(epi): update 5.16 Generate Nonuniform Random Numbers

--- epi-python/nonuniform_random_number.py
+++ epi-python/nonuniform_random_number.py
@@ -10,10 +10,39 @@ But I couldn't figure out how to translate that into code, without making a big 
 So, I decided to not pursue it further.
 """
 
-def nonuniform_random_number_generation(values: List[int], probabilities: List[float]) -> int:
-    prefix_probabilities = list(itertools.accumulate(probabilities)) # creates the interval
-    random_index = bisect.bisect(prefix_probabilities, random.random()) # pick a random number 
-    # The random() method returns a random floating number between 0 and 1. 
-    # Syntax: random.random() 
+from typing import List
+import bisect, itertools, random
+
+
+def nonuniform_random_number_generation(
+    values: List[int], probabilities: List[float]
+) -> int:
+    prefix_probabilities = list(
+        itertools.accumulate(probabilities)
+    )  # creates the interval
+    random_index = bisect.bisect(
+        prefix_probabilities, random.random()
+    )  # pick a random number
+    # The random() method returns a random floating number between 0 and 1.
+    # Syntax: random.random()
     # Parameter Values: No parameters.
     return values[random_index]
+
+
+# more straightforward
+
+
+def nonuniform_random_number_generation(
+    values: List[int], probabilities: List[float]
+) -> int:
+    for i in range(1, len(probabilities)):
+        probabilities[i] += probabilities[i - 1]
+
+    rand_range = random.random()
+    index = 0
+    for i in range(len(probabilities)):
+        if probabilities[i] > rand_range:
+            index = i
+            break
+
+    return values[index]
