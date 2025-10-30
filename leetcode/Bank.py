c2f6f76765c7841812d00707efac039347227365
natashalukmanto <natshaaudrey@gmail.com>
2025-10-29 23:41:23 -0700

    feat(lc): solve 2043. Simple Bank System

---
+++ leetcode/Bank.py
@@ -0,0 +1,46 @@
+from typing import List
+
+class Bank:
+    # [30,100,10,50,10]
+    def __init__(self, balance: List[int]):
+        self.account = []
+        self.n = len(balance)
+        for b in balance:
+            self.account.append(b)
+
+    def isValidAccount(self, account: int) -> bool:
+        return 1 <= account <= self.n
+
+    def isValidAmount(self, account: int, money: int) -> bool:
+        return money <= self.account[account - 1]
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        valid = (
+            self.isValidAccount(account1)
+            and self.isValidAccount(account2)
+            and self.isValidAmount(account1, money)
+        )
+        if valid:
+            self.account[account1 - 1] -= money
+            self.account[account2 - 1] += money
+        return valid
+
+    def deposit(self, account: int, money: int) -> bool:
+        valid = self.isValidAccount(account)
+        if valid:
+            self.account[account - 1] += money
+        return valid
+
+    def withdraw(self, account: int, money: int) -> bool:
+        valid = self.isValidAccount(account) and self.isValidAmount(account, money)
+        # print(self.account[account - 1], money, valid)
+        if valid:
+            self.account[account - 1] -= money
+        return valid
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
