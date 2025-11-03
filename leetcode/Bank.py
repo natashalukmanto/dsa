1c672afe6124f0d2231466f02c3735192fd64c5a
natashalukmanto <natshaaudrey@gmail.com>
2025-11-02 23:37:36 -0800

    fix(lc): add another approach to 2043. Simple Bank System

--- leetcode/Bank.py
+++ leetcode/Bank.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Bank:
     # [30,100,10,50,10]
     def __init__(self, balance: List[int]):
@@ -37,6 +38,48 @@ class Bank:
         if valid:
             self.account[account - 1] -= money
         return valid
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
+
+
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = [0] + balance
+
+    def isValidAccount(self, account: int) -> bool:
+        return 1 <= account < len(self.balance)
+
+    def isValidTransaction(self, account: int, money: int) -> bool:
+        return self.balance[account] >= money
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if (
+            self.isValidAccount(account1)
+            and self.isValidAccount(account2)
+            and self.isValidTransaction(account1, money)
+        ):
+            self.balance[account1] -= money
+            self.balance[account2] += money
+            return True
+        return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        if self.isValidAccount(account):
+            self.balance[account] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if self.isValidAccount(account) and self.isValidTransaction(account, money):
+            self.balance[account] -= money
+            return True
+        return False
 
 
 # Your Bank object will be instantiated and called as such:
