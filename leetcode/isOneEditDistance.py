09e76a3fc351108912e666c60e860bd36d073be2
natashalukmanto <natshaaudrey@gmail.com>
2025-11-03 18:07:47 -0800

    feat(lc): add better approach to 161. One Edit Distance

--- leetcode/isOneEditDistance.py
+++ leetcode/isOneEditDistance.py
@@ -1,5 +1,21 @@
 from collections import Counter
 
+def isOneEditDistance(self, s: str, t: str) -> bool:
+    if len(s) > len(t):
+        return self.isOneEditDistance(t, s)
+    
+    if len(t) - len(s) > 1:
+        return False
+    
+    for i in range(len(s)):
+        if s[i] != t[i]:
+            if len(s) == len(t):
+                return s[i+1:] == t[i+1:]
+            else:
+                return s[i:] == t[i+1:]
+        
+    return s != t
+
 def isOneEditDistance(s: str, t: str) -> bool:
     if s == t or abs(len(s) - len(t)) > 1: return False
     
