042e5fa9e31da30badb2e05ef1dd0a361ad39775
natashalukmanto <natshaaudrey@gmail.com>
2025-11-09 15:57:48 -0800

    feat(lc): solve 442. Find All Duplicates in an Array

--- leetcode/findDuplicates.py
+++ leetcode/findDuplicates.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def findDuplicates(self, nums: List[int]) -> List[int]:
+def findDuplicates(nums: List[int]) -> List[int]:
     res = []
 
     for num in nums:
@@ -11,4 +11,15 @@ def findDuplicates(self, nums: List[int]) -> List[int]:
         else:
             nums[index] *= -1
 
+    return res
+
+
+def findDuplicates0(nums: List[int]) -> List[int]:
+    res, seen = [], set()
+
+    for num in nums:
+        if num not in seen:
+            seen.add(num)
+        else:
+            res.append(num)
     return res
