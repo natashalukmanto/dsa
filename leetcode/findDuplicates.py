5945a5021d9adc7d5fd2678280831640a50b0ec7
natashalukmanto <natshaaudrey@gmail.com>
2025-11-09 15:57:41 -0800

    feat(lc): solve 442. Find All Duplicates in an Array

---
+++ leetcode/findDuplicates.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def findDuplicates(self, nums: List[int]) -> List[int]:
+    res = []
+
+    for num in nums:
+        index = abs(num) - 1
+        if nums[index] < 0:
+            res.append(abs(num))
+        else:
+            nums[index] *= -1
+
+    return res
