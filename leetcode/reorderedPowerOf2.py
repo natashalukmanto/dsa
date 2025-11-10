d74417d81392248f88c1321af3ac332f22830e32
natashalukmanto <natshaaudrey@gmail.com>
2025-11-09 17:23:49 -0800

    feat(lc): solve 869. Reordered Power of 2

---
+++ leetcode/reorderedPowerOf2.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+def reorderedPowerOf2(n: int) -> bool:
+    n = list(str(n))
+    n.sort()
+
+    for i in range(30):
+        x = list(str((1 << i)))
+        x.sort()
+        if x == n:
+            return True
+
+    return False
+
+
+def reorderedPowerOf2(n: int) -> bool:
+    count = Counter(str(n))
+
+    for i in range(30):
+        if count == Counter(str(1 << i)):
+            return True
+
+    return False
