fcbe7c52a30e5ddc36ec6b2ce0c19ae0fb546384
natashalukmanto <natshaaudrey@gmail.com>
2025-11-12 23:35:54 -0800

    feat(epi): solve 10.3 Sort An Almost-Sorted Array

---
+++ epi-python/sort_almost_sorted_array.py
@@ -0,0 +1,42 @@
+from typing import Iterator, List
+import itertools, heapq
+
+
+def sort_approximately_sorted_array0(sequence: Iterator[int], k: int) -> List[int]:
+    res, min_heap = [], []
+
+    for x in itertools.islice(sequence, k):
+        heapq.heappush(min_heap, x)
+
+    for x in sequence:
+        val = heapq.heappushpop(min_heap, x)
+        res.append(val)
+
+    while min_heap:
+        res.append(heapq.heappop(min_heap))
+
+    return res
+
+
+def sort_approximately_sorted_array0(sequence: Iterator[int], k: int) -> List[int]:
+    res, min_heap = [], []
+
+    for _ in range(k + 1):
+        try:
+            item = next(sequence)
+            heapq.heappush(min_heap, item)
+        except StopIteration:
+            break
+
+    while True:
+        res.append(heapq.heappop(min_heap))
+        try:
+            item = next(sequence)
+            heapq.heappush(min_heap, item)
+        except StopIteration:
+            break
+
+    while min_heap:
+        res.append(heapq.heappop(min_heap))
+
+    return res
