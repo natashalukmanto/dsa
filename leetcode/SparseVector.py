3e9b5311ea8133a33ad48c52d81e01414e266855
natashalukmanto <natshaaudrey@gmail.com>
2025-11-17 16:33:23 -0800

    feat(lc): add alternative solutions to 1570. Dot Product of Two Sparse Vectors

--- leetcode/SparseVector.py
+++ leetcode/SparseVector.py
@@ -17,3 +17,44 @@ class SparseVector:
 # v1 = SparseVector(nums1)
 # v2 = SparseVector(nums2)
 # ans = v1.dotProduct(v2)
+
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.vector = {}
+
+        for i, num in enumerate(nums):
+            if num != 0:
+                self.vector[i] = num
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        result = 0
+        for i, num in self.vector.items():
+            if i in vec.vector:
+                result += num * vec.vector[i]
+        return result
+
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.vector = []
+
+        for i, n in enumerate(nums):
+            if n != 0:
+                self.vector.append((i, n))
+            
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        result = 0
+        p, q = 0, 0
+
+        while p < len(vec.vector) and q < len(self.vector):
+            if vec.vector[p][0] == self.vector[q][0]:
+                result += vec.vector[p][1] * self.vector[q][1]
+                p += 1
+                q += 1
+            elif vec.vector[p][0] < self.vector[q][0]:
+                p += 1
+            else:
+                q += 1
+        
+        return result
