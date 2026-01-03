231e022e9faf1e01f42b860c1fbc0373995665c3
natashalukmanto <natshaaudrey@gmail.com>
2026-01-03 15:19:42 +0700

    feat(epi): add another version to 5.2 Increment an Arbitrary-Precision Integer

--- epi-python/int_as_array_increment.py
+++ epi-python/int_as_array_increment.py
@@ -14,6 +14,22 @@ def plus_one(A: List[int]) -> List[int]:
         
     return A
 
+def plus_one(A: List[int]) -> List[int]:
+    A[-1] += 1
+    
+    if A[-1] != 10: return A
+    
+    for i in range(len(A)-1, 0, -1):
+        if A[i] == 10:
+            A[i-1] += 1
+            A[i] = 0
+        
+    if A[0] == 10:
+        A.append(0)
+        A[0] = 1
+    
+    return A
+
 A = [1, 2, 9]
 print(plus_one(A)) # [1, 3, 0]
 B = [9, 9, 9, 9]
