64383ca93ca50f1f6ad0ff6af41884c4296c8d43
natashalukmanto <natshaaudrey@gmail.com>
2026-01-15 14:07:01 +0700

    feat(lc): solve 424. Longest Repeating Character Replacement

--- leetcode/characterReplacement.py
+++ leetcode/characterReplacement.py
@@ -15,3 +15,24 @@ def characterReplacement(s: str, k: int) -> int:
     return max_len
 
     # AABABBA
+
+"""
+2nd try:
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = max_freq = max_len = 0
+        counter = {}
+
+        for right in range(len(s)):
+            counter[s[right]] = counter.get(s[right], 0) + 1
+            max_freq = max(max_freq, counter[s[right]])
+
+            while (right - left + 1) - max_freq > k:
+                counter[s[left]] -= 1
+                left += 1
+
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
+        
+"""
