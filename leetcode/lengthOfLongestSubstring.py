84fb81eca1bf035ddddc737b525da5620ca0c020
natashalukmanto <natshaaudrey@gmail.com>
2026-01-23 13:52:48 -0800

    retry(lc): retry 3. Longest Substring Without Repeating Characters

--- leetcode/lengthOfLongestSubstring.py
+++ leetcode/lengthOfLongestSubstring.py
@@ -11,3 +11,18 @@ def lengthOfLongestSubstring(s: str) -> int:
         max_len = max(max_len, right - left + 1)
 
     return max_len
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    left = 0
+    max_len = 0
+    my_set = set()
+
+    for right in range(len(s)):
+        while s[right] in my_set:
+            my_set.remove(s[left])
+            left += 1
+        my_set.add(s[right])
+        max_len = max(max_len, right - left + 1)
+
+    return max_len
