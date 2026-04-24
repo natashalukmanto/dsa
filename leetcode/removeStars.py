3a387df11426bff3e992d73c3388943799224018
natashalukmanto <natshaaudrey@gmail.com>
2026-04-24 15:05:20 -0700

    feat(lc): solve 2390. Removing Stars From a String

---
+++ leetcode/removeStars.py
@@ -0,0 +1,10 @@
+def removeStars(self, s: str) -> str:
+    stack = []
+
+    for char in s:
+        if stack and char == "*":
+            stack.pop()
+        else:
+            stack.append(char)
+
+    return "".join(stack)
